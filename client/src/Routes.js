8cb9e464bc777f94983d2876faa44ec5caf4fc32
Wenfei Zhou <wzhou2046@gmail.com>
2020-05-06 18:18:02 -0400

    added new pages:branding, event logs, and settings.
    Co-authored-by: Rachel Quatararo <rquartararo@gmail.com>

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './components/core/Login';
 import NavBar from './components/core/NavBar';
+import Footer from './components/core/Footer';
+
+//admin components
 import AdminDashboard from './components/user/AdminDashboard';
-import Footer from './components/core/Footer'
+import EventLogs from './components/user/EventLogs';
+import Settings from './components/user/Settings';
+
+//private/admin routes
 import AdminRoute from './components/AdminRoute';
-
-
+import AccountBranding from './components/user/AccountBranding';
 
 const Routes = () => {
   return (
@@ -14,7 +19,10 @@ const Routes = () => {
       <NavBar />
       <Switch>
         <Route path='/' exact component={Login} />
-        <AdminRoute path='/orgAdmin' component={AdminDashboard} />
+        <AdminRoute path='/admin' exact component={AdminDashboard} />
+        <AdminRoute path='/admin/branding' exact component={AccountBranding} />
+        <AdminRoute path='/admin/eventlogs' exact component={EventLogs} />
+        <AdminRoute path='/admin/settings' exact component={Settings} />
       </Switch>
       <Footer />
     </BrowserRouter>
--- client/src/components/user/AdminDashboard.js
+++ client/src/components/user/AdminDashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AdminDashboard = () => {
   return (
@@ -6,6 +7,15 @@ const AdminDashboard = () => {
       <h1>Manage Teams</h1>
       <button>Add Member</button>
       <button>Add Team</button>
+      <Link to='/admin/settings'>
+        <button>Settings</button>
+      </Link>
+      <Link to='/admin/branding'>
+        <button>Account Branding</button>
+      </Link>
+      <Link to='/admin/eventlogs'>
+        <button>Event Logs</button>
+      </Link>
     </div>
   );
 };
